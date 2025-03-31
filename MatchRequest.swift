ac09a0d0c738a28d39dea0231bc2c132c0423667
Runa3313 <2pbc6k@gmail.com>
2025-03-31 12:52:23 -0700

    Update MatchRequest.swift
    2nd

--- MatchRequest.swift
+++ MatchRequest.swift
@@ -1,8 +1,9 @@
 
-import Foundation
+import SwiftUI
 
+// HomeView とは別にグローバルスコープで定義
 struct MatchRequest: Identifiable {
-    let id: UUID
+    let id = UUID()
     let dogName: String
     let requesterName: String
     var isApproved: Bool
